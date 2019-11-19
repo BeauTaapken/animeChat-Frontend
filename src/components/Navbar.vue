4a31bb0592b322dc801329e1f0a4d352af93a770
BeauTaapken <beautaapken@me.com>
2019-11-19 18:03:39 +0100

    * Added vuetify to the application * Added all needed pages to the application * Added checks if you are logged in or not

---
+++ src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <v-toolbar
+      :extended="extended"
+      :prominent="prominent"
+      :dense="dense"
+      :collapse="collapse"
+      :flat="flat"
+      :src="
+        bg
+          ? 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'
+          : undefined
+      "
+      :extension-height="extensionHeight"
+    >
+
+      <v-toolbar-title>{{username}}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn to="/animeandmanga" text>Anime & manga</v-btn>
+        <v-btn to="/textchat" text>Textchat</v-btn>
+        <v-btn to="/webcamchat" text>Videochat</v-btn>
+        <v-btn to="/users" text>MAL users</v-btn>
+      </v-toolbar-items>
+
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn icon>
+          <v-icon>mdi-power-settings</v-icon>
+        </v-btn>
+      </template>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      username: null
+    };
+  },
+  mounted(){
+    try{
+      this.username = JSON.parse(sessionStorage.getItem("userInfo")).profile.name;
+    }
+    catch (e) {
+      this.username = null;
+    }
+  },
+  watch: {
+    '$route'(){
+      try{
+        this.username = JSON.parse(sessionStorage.getItem("userInfo")).profile.name;
+      }
+      catch (e) {
+        this.username = null;
+      }
+    }
+  }
+};
+</script>
